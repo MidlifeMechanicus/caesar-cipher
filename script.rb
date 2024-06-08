003a8eba85e6f36540228a0a39f0e5899b3bdbe4
ElectronDragon <electricdragondevelopment@gmail.com>
2024-06-08 07:41:47 +0100

    Get string and shift from user, create modulus function for lange shift, and refactor

--- script.rb
+++ script.rb
@@ -8,32 +8,40 @@
     #remember to wrap!
 # Convert shift value back to letters
 # Return new string
-string = "Hello, my friend."
-shift = 5
-string.chars.map{|i| i.ord}
 
-def create_cipher (string, shift)
-    number_sequence = string.chars.map do |letter|
-        if letter.ord.between?(65, 90)
-            if letter.ord.between?(65, 90-shift)
-                letter.ord + shift
-            else
-                letter.ord + shift - 90 + 65
-            end
-        elsif letter.ord.between?(97, 122)
-            if letter.ord.between?(97, 122-shift)
-                letter.ord + shift
-            else
-                letter.ord + shift - 122 + 97
-            end
-        else
-            letter.ord
-        end
-    end
+puts "What would you like to say?"
+string = gets.chomp
+puts "How far would you like to shift?"
+shift = gets.chomp.to_i
 
-    letter_sequence = number_sequence.map do |number|
-        number.chr
-    end
-
-    new_string =  letter_sequence.join
+if shift >= 26
+    shift = shift % 26
 end
+
+number_sequence = string.chars.map do |letter|
+    if letter.ord.between?(65, 90)
+        if letter.ord.between?(65, 90-shift)
+            letter.ord + shift
+        else
+            letter.ord + shift - 90 + 65
+        end
+    elsif letter.ord.between?(97, 122)
+        if letter.ord.between?(97, 122-shift)
+            letter.ord + shift
+        else
+            letter.ord + shift - 122 + 97
+        end
+    else
+        letter.ord
+    end
+end
+
+letter_sequence = number_sequence.map do |number|
+    number.chr
+end
+
+new_string =  letter_sequence.join
+
+puts "Your cipher is:"
+puts new_string
+
