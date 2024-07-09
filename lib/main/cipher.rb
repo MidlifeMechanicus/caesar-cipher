d56298f3d9fb7f9c8101d791f6221c4ba5f4e3fb
ElectronDragon <electricdragondevelopment@gmail.com>
2024-07-09 10:29:58 +0100

    Write #encode and past rspec tests

--- lib/main/cipher.rb
+++ lib/main/cipher.rb
@@ -3,6 +3,13 @@
 class Cipher
 
   def intialize
+    # Another approach would be to set attributes such as @string, @shift and @cipher.
+  end
+
+  def encode
+    string = get_string
+    shift = get_shift
+    get_cipher(string, shift)
   end
 
   def get_string
@@ -17,7 +24,30 @@ class Cipher
     shift >= 26 ? shift % 26 : shift
   end
 
-  def add(a, b)
-    a + b
+  def get_cipher(string, shift)
+    number_sequence = string.chars.map do |letter|
+      if letter.ord.between?(65, 90)
+        if letter.ord.between?(65, 90 - shift)
+          letter.ord + shift
+        else
+          letter.ord + shift - 90 + 64
+        end
+      elsif letter.ord.between?(97, 122)
+        if letter.ord.between?(97, 122 - shift)
+          letter.ord + shift
+        else
+          letter.ord + shift - 122 + 96
+        end
+      else
+        letter.ord
+      end
+    end
+    
+    letter_sequence = number_sequence.map do |number|
+      number.chr
+    end
+    
+    new_string = letter_sequence.join
   end
+
 end
--- spec/cipher_spec.rb
+++ spec/cipher_spec.rb
@@ -3,6 +3,17 @@
 require "./lib/main/cipher"
 
 describe Cipher do
+
+  describe "#encode" do
+    it "returns 'YZA'" do
+      cipher = Cipher.new
+      # cipher.stub(:gets).and_return("Hello World")
+      allow(cipher).to receive(:gets).and_return("XYZ", "1")
+      expect(cipher.encode).to eq("YZA")
+    end
+  end
+
+
   describe "#get_string" do
     it "returns Hello World as an input" do
       cipher = Cipher.new
@@ -33,10 +44,17 @@ describe Cipher do
     end
   end
 
-  describe "#add" do
-    it "returns the sum of two numbers" do
+  describe "#get_cipher" do
+    it "returns 'cbd'" do
       cipher = Cipher.new
-      expect(cipher.add(5, 2)).to eq(7)
+      expect(cipher.get_cipher("abc", 1)).to eq("bcd")
     end
+
+    it "returns 'yza'" do
+      cipher = Cipher.new
+      expect(cipher.get_cipher("xyz", 1)).to eq("yza")
+    end
+
   end
+
 end
